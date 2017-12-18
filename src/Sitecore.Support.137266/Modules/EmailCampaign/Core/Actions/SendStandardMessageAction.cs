50ef2145abf3b851e1e7402b38b806303fe56b6c
DK\msb <msb@sitecore.net>
2017-12-18 13:36:19 -0500

    Update EcmClientService implementation in order to apply patch on scale

--- src/Sitecore.Support.137266/Modules/EmailCampaign/Core/Actions/SendStandardMessageAction.cs
+++ src/Sitecore.Support.137266/Modules/EmailCampaign/Core/Actions/SendStandardMessageAction.cs
@@ -1,52 +1,60 @@
 ﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.Actions
 {
-  using Sitecore.Analytics.Automation;
-  using Sitecore.Analytics.Tracking;
-  using Sitecore.Diagnostics;
-  using Sitecore.ExM.Framework.Diagnostics;
-  using Sitecore.Modules.EmailCampaign;
-  using Sitecore.Modules.EmailCampaign.Exceptions;
-  using Sitecore.Modules.EmailCampaign.Xdb;
-  using System;
+    using Sitecore.Analytics.Automation;
+    using Sitecore.Analytics.Tracking;
+    using Sitecore.Diagnostics;
+    using Sitecore.ExM.Framework.Diagnostics;
+    using Sitecore.Modules.EmailCampaign;
+    using Sitecore.Modules.EmailCampaign.Core;
+    using Sitecore.Modules.EmailCampaign.Exceptions;
+    using Sitecore.Modules.EmailCampaign.Xdb;
+    using System;
 
-  public class SendStandardMessageAction : IAutomationAction
-  {
-    private readonly ILogger logger;
-
-    public SendStandardMessageAction() : this(Logger.Instance)
+    public class SendStandardMessageAction : IAutomationAction
     {
-    }
+        private readonly ILogger logger;
 
-    public SendStandardMessageAction(ILogger logger)
-    {
-      Assert.ArgumentNotNull(logger, "logger");
-      this.logger = logger;
-    }
+        public SendStandardMessageAction() : this(Logger.Instance)
+        {
+        }
 
-    public AutomationActionResult Execute(AutomationActionContext automationStatesContext)
-    {
-      Assert.ArgumentNotNull(automationStatesContext, "automationStatesContext");
-      string g = automationStatesContext.Parameters["StandardMessageId"];
-      Contact contact = automationStatesContext.Contact;
-      if ((contact == null) || string.IsNullOrEmpty(contact.Identifiers.Identifier))
-      {
-        this.LogError("An engagement automation plan action failed as Email Experience Manager could not find the username associated with the action.");
-        return AutomationActionResult.Continue;
-      }
-      try
-      {
-        new Sitecore.Support.Modules.EmailCampaign.AsyncSendingManager(Factory.GetMessage(g)).SendStandardMessage(new XdbContactId(contact.ContactId));
-      }
-      catch (EmailCampaignException exception)
-      {
-        this.logger.LogError(exception);
-      }
-      return AutomationActionResult.Continue;
-    }
+        public SendStandardMessageAction(ILogger logger)
+        {
+            Assert.ArgumentNotNull(logger, "logger");
+            this.logger = logger;
+        }
 
-    private void LogError(string error)
-    {
-      this.logger.LogError("SendStandardMessageAction: " + error);
+        public AutomationActionResult Execute(AutomationActionContext automationStatesContext)
+        {
+            Assert.ArgumentNotNull(automationStatesContext, "automationStatesContext");
+            string g = automationStatesContext.Parameters["StandardMessageId"];
+            Contact contact = automationStatesContext.Contact;
+            if ((contact == null) || string.IsNullOrEmpty(contact.Identifiers.Identifier))
+            {
+                this.LogError("An engagement automation plan action failed as Email Experience Manager could not find the username associated with the action.");
+                return AutomationActionResult.Continue;
+            }
+            try
+            {
+                if (Factory.GetClientApiInstance() is ClientApiLocal)
+                {
+                    new Sitecore.Support.Modules.EmailCampaign.AsyncSendingManager(Factory.GetMessage(g)).SendStandardMessage(new XdbContactId(contact.ContactId));
+                }
+                else
+                {
+                    ClientApi.SendStandardMessage(new Guid(g), new XdbContactId(contact.ContactId), true);
+                }
+            }
+            catch (EmailCampaignException exception)
+            {
+                this.logger.LogError(exception);
+            }
+            return AutomationActionResult.Continue;
+        }
+
+        private void LogError(string error)
+        {
+            this.logger.LogError("SendStandardMessageAction: " + error);
+        }
     }
-  }
 }
---
+++ src/Sitecore.Support.137266/sitecore modules/web/EmailCampaign/ECMClientService.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Modules.EmailCampaign;
+using Sitecore.Modules.EmailCampaign.Recipients;
+using System;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace Sitecore.Support.EmailCampaign.Cm.UI.sitecore_modules.Web.EmailCampaign
+{
+    public class ECMClientService : Sitecore.EmailCampaign.Cm.UI.sitecore_modules.Web.EmailCampaign.ECMClientService
+    {
+        [SoapHeader("AuthenticationInformation"), WebMethod(EnableSession = true)]
+        public new void SendStandardMessage(Guid messageId, string recipientId, bool async)
+        {
+            RecipientId id = RecipientRepository.GetDefaultInstance().ResolveRecipientId(recipientId);
+            if (id != null)
+            {
+                if (async)
+                {
+                    new Sitecore.Support.Modules.EmailCampaign.AsyncSendingManager(Factory.GetMessage(messageId.ToString())).SendStandardMessage(id);
+                }
+                else
+                {
+                    new Sitecore.Support.Modules.EmailCampaign.SendingManager(Factory.GetMessage(messageId.ToString())).SendStandardMessage(id);
+                }
+            }
+        }
+    }
+}
